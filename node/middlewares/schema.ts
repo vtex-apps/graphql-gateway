aca4b571315a5fe1d5d4cccc02592c3a2a9171c2
Emerson Laurentino <emerson@laurentino.co>
2021-05-11 20:56:09 -0300

    chore: remove checkout and search name types

--- node/middlewares/schema.ts
+++ node/middlewares/schema.ts
@@ -32,7 +32,7 @@ const apps = [
     app: 'vtex.search-resolver@1.x',
     executor: getExecutorForApp,
     transforms: [
-      new RenameTypes(name => `${typeName}_SearchResolver_${name}`),
+      new RenameTypes(name => `${typeName}_${name}`),
       new NamespaceUnderFieldTransform(typeName, fieldName),
     ],
   },
@@ -40,7 +40,7 @@ const apps = [
     app: 'vtex.checkout-graphql@0.x',
     executor: getExecutorForApp,
     transforms: [
-      new RenameTypes(name => `${typeName}_CheckoutGraphql_${name}`),
+      new RenameTypes(name => `${typeName}_${name}`),
       new NamespaceUnderFieldTransform(typeName, fieldName),
     ],
   },
